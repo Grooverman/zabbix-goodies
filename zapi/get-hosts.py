51f7d9c0a649b2247e60fa2af94d0092e73181bf
Ubuntu <rudolf.klaus@zohomail.eu>
2022-07-06 18:14:19 +0000

    add zapi scripts

---
+++ zapi/get-hosts.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import json
+import yaml
+from pprint import pformat
+from pyzabbix import ZabbixAPI
+from config import *
+
+# call api
+zapi = ZabbixAPI(
+        url=REMOTE_ZABBIX_URL,
+        user=REMOTE_ZABBIX_USER,
+        password=REMOTE_ZABBIX_PASSWORD)
+hosts = zapi.host.get(
+        search='%%',
+        monitored_hosts=True,
+#        with_monitored_triggers=True,
+        selectHttpTests=True,
+        output=['host', 'httpTests'])
+
+#print(json.dumps(hosts))
+print(yaml.dump(hosts))
+exit()
+
+
