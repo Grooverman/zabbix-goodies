38cd9cfbdc6142ac9569044e38f51f39651545ca
Ubuntu <rudolf.klaus@zohomail.eu>
2022-08-04 14:32:57 +0000

    new kids on the block

---
+++ zapi/agent-unavailable-hosts.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import json
+import yaml
+from pprint import pformat
+from pyzabbix import ZabbixAPI
+from config import *
+
+# call api
+zapi = ZabbixAPI(
+        url=REMOTE_ZABBIX_URL,
+        user=REMOTE_ZABBIX_USER,
+        password=REMOTE_ZABBIX_PASSWORD)
+hosts = zapi.host.get(
+        filter={'available': 2, 'status': 0},
+        selectInterfaces=["type"],
+        output=['interfaces', 'host', 'status'])
+
+#print(json.dumps(hosts))
+print(yaml.dump(hosts))
+exit()
+
--- zapi/problems_with_host.py
+++ zapi/problems_with_host.py
@@ -13,10 +13,27 @@ zapi = ZabbixAPI(
         password=REMOTE_ZABBIX_PASSWORD)
 zapi_output = zapi.problem.get(
         sortfield=["eventid"],
+        selectTags='extend',
         output=["clock", "objectid", "name", "severity"])
+
+for i in zapi_output:
+    i['responsible'] = ''
+    i['service'] = ''
+    for t in i['tags']:
+        tag = t['tag']
+        value = t['value']
+        try:
+            if tag == 'Responsible':
+                i.update({'responsible': value})
+            if tag == 'Service':
+                i.update({'service': value})
+        except:
+            pass
+    del i['tags']
 
 #print(yaml.dump(zapi_output))
 #print(json.dumps(zapi_output))
+#exit()
 
 # make list of eventids
 eventids = []
@@ -25,7 +42,7 @@ for i in zapi_output:
 
 # get hosts for each eventid
 zapi_events_host = zapi.event.get(
-        selectHosts=['hostd', 'host'],
+        selectHosts=['hostid', 'host'],
         eventids=eventids,
         output='eventid')
 
@@ -46,7 +63,8 @@ for i in zapi_events_host:
         e = {host_event_id: event}
         hostid_eventid_list.append(e)
 
-print('¨eventid¨, ¨clock¨, ¨objectid¨, ¨name¨, ¨severity¨, ¨hostid¨, ¨host¨, ¨date¨')
+# print in csv format
+print('¨eventid¨, ¨clock¨, ¨objectid¨, ¨name¨, ¨severity¨, ¨responsible¨, ¨service¨, ¨hostid¨, ¨host¨, ¨date¨')
 for i in hostid_eventid_list:
     v = ''
     event_with_host = [v for k,v in i.items()][0]
